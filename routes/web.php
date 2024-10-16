e0346ac0b384dd0b11b2f1bd9c4032af3a5e0652
velove1 <danielleraay@gmail.com>
2024-10-16 10:49:37 +0700

    laravel intial commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); // menuju ke file welcome.blade.php
+});
+
+Route::get('/login', function () {
+    return view('login'); // menuju ke file welcome.blade.php
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); // menuju ke file welcome.blade.php
+});
+
+Route::get('/about', function () {
+    return "Tentang Gue"; // menuju ke file text Tentang Que
 });
